0b0a2a974da53e295b16332ab37fe4da35c20b65
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-01-15 21:34:52 +0400

    initiate deploying project

---
+++ logger.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from datetime import datetime
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def configure_logger(logger_name, log_file_path):
+    """
+    Configure and return a logger.
+
+    :param logger_name: Name of the logger.
+    :param log_file_path: File path for the log file.
+    :return: Configured logger.
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    log_handler = logging.FileHandler(log_file_path)
+    # %(asctime)s
+    log_formatter = logging.Formatter('%(levelname)s %(message)s')
+    log_handler.setFormatter(log_formatter)
+
+    logger.addHandler(log_handler)
+    return logger
+
+
+# Log path from configuration
+log_path = config['General']['LOG_PATH']
+
+# Configuring a logger for the main module
+main_logger = configure_logger(__name__, log_path)
